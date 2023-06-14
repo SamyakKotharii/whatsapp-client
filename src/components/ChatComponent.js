3f938496412559a317b850357449993b345e119a
SamyakKotharii <shkothari904.sk@gmail.com>
2023-06-14 21:52:38 +0530

    Modified

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -24,7 +24,6 @@ const ChatComponent = ({ selectedNumber }) => {
 
   useEffect(() => {
     fetchMessages();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedNumber]);
 
   const handleSubmit = async (e) => {
