fd917920d8453014fc001893b7e5d6ffecae1026
SamyakKotharii <shkothari904.sk@gmail.com>
2023-06-15 16:59:10 +0530

    Modified

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -1,4 +1,78 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import ChatConversation from "./ChatConversation";
+// import "../index.css";
+
+// const ChatComponent = ({ selectedNumber }) => {
+//   const [body, setBody] = useState("");
+//   const [messages, setMessages] = useState([]);
+//   const [error, setError] = useState(null);
+
+//   const fetchMessages = async () => {
+//     try {
+//       const response = await axios.get(
+//         `https://darkhorsestockscloud.onrender.com/messages/${selectedNumber}`
+//       );
+//       console.log("Response", response.data);
+//       setMessages(response.data);
+//       setError(null); // Clear any previous error
+//     } catch (error) {
+//       console.error("An error occurred:", error);
+//       setError("Failed to fetch messages. Please try again."); // Set error message
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchMessages();
+//   }, [selectedNumber]);
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       await axios.post(
+//         "https://darkhorsestockscloud.onrender.com/send-message",
+//         {
+//           to: selectedNumber,
+//           text: {
+//             preview_url: false,
+//             body: body,
+//           },
+//         }
+//       );
+//       setBody("");
+//       fetchMessages();
+//       setError(null); // Clear any previous error
+//     } catch (error) {
+//       console.error("An error occurred:", error);
+//       setError("Failed to send message. Please try again."); // Set error message
+//     }
+//   };
+
+//   return (
+//     <div className="chat-component">
+//       <div className="message-container">
+//         {selectedNumber && (
+//           <div className="selected-number">{selectedNumber}</div>
+//         )}
+//         {error && <div className="error">{error}</div>}{" "}
+//         {/* Display error message */}
+//         <ChatConversation messages={messages} />
+//       </div>
+//       <form onSubmit={handleSubmit} className="message-input">
+//         <input
+//           type="text"
+//           value={body}
+//           onChange={(e) => setBody(e.target.value)}
+//           placeholder="Type a message..."
+//         />
+//         <button type="submit">Send</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default ChatComponent;
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import ChatConversation from "./ChatConversation";
 import "../index.css";
@@ -8,7 +82,7 @@ const ChatComponent = ({ selectedNumber }) => {
   const [messages, setMessages] = useState([]);
   const [error, setError] = useState(null);
 
-  const fetchMessages = async () => {
+  const fetchMessages = useCallback(async () => {
     try {
       const response = await axios.get(
         `https://darkhorsestockscloud.onrender.com/messages/${selectedNumber}`
@@ -20,11 +94,11 @@ const ChatComponent = ({ selectedNumber }) => {
       console.error("An error occurred:", error);
       setError("Failed to fetch messages. Please try again."); // Set error message
     }
-  };
+  }, [selectedNumber]);
 
   useEffect(() => {
     fetchMessages();
-  }, [selectedNumber]);
+  }, [selectedNumber, fetchMessages]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -54,8 +128,7 @@ const ChatComponent = ({ selectedNumber }) => {
         {selectedNumber && (
           <div className="selected-number">{selectedNumber}</div>
         )}
-        {error && <div className="error">{error}</div>}{" "}
-        {/* Display error message */}
+        {error && <div className="error">{error}</div>}
         <ChatConversation messages={messages} />
       </div>
       <form onSubmit={handleSubmit} className="message-input">
